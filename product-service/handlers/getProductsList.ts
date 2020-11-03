a34fee9b322e0592dc0c7b039b75314d74d6a2d5
Pavel Zaletski <pavel_zaletski@epam.com>
2020-11-03 16:13:47 +0300

    refactor: Error handling updates

--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -4,18 +4,32 @@ import { getProducts } from '../src/dataProvider';
 
 
 export const getProductsList: APIGatewayProxyHandler = async (event, _context) => {
+  const headers = {
+    "Access-Control-Allow-Headers" : "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+  };
+
   try {
     const data = await getProducts();
+    if (data) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data, null, 2),
+        headers,
+      };
+    }
+
     return {
-      statusCode: 200,
-      body: JSON.stringify(data, null, 2),
-      headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-      },
+      statusCode: 404,
+      body: 'not found',
+      headers,
     };
   } catch (err) {
-    return err;
+    return {
+      statusCode: 500,
+      body: err,
+      headers,
+    };
   }
 }
